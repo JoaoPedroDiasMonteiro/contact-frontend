39bf95ec8c1df5167540fb14d53a41b993c9237f
João Pedro Dias <joaopedro453@hotmail.com>
2024-06-01 15:28:41 -0300

    Add Modal component

---
+++ src/components/Ui/Modal.tsx
@@ -0,0 +1,65 @@
+import { Dialog, DialogPanel, Transition, TransitionChild } from '@headlessui/react'
+import { XMarkIcon } from '@heroicons/react/24/outline'
+import { Dispatch, SetStateAction } from 'react'
+
+interface ModalProps extends React.HTMLAttributes<HTMLDivElement> {
+  readonly open: boolean,
+  readonly setOpen: Dispatch<SetStateAction<boolean>>
+}
+
+export default function Modal({ children, open, setOpen, ...rest }: ModalProps) {
+  function close() {
+    setOpen(false)
+  }
+
+  return (
+    <Transition show={open}>
+      <Dialog className="relative z-10" onClose={close}>
+        <TransitionChild
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 hidden bg-gray-500 bg-opacity-75 transition-opacity md:block" />
+        </TransitionChild>
+
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+          <div className="flex min-h-full items-stretch justify-center text-center md:items-center md:px-2 lg:px-4">
+            {/* This element is to trick the browser into centering the modal contents. */}
+            <span className="hidden md:inline-block md:h-screen md:align-middle" aria-hidden="true">
+              &#8203;
+            </span>
+            <TransitionChild
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 md:translate-y-0 md:scale-95"
+              enterTo="opacity-100 translate-y-0 md:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 md:scale-100"
+              leaveTo="opacity-0 translate-y-4 md:translate-y-0 md:scale-95"
+            >
+              <DialogPanel className="flex w-full transform text-left text-base transition md:my-8 md:max-w-2xl md:px-4 lg:max-w-4xl">
+                <div className="relative flex w-full items-center overflow-hidden bg-white px-4 pb-8 pt-14 shadow-2xl sm:px-6 sm:pt-8 md:p-6 lg:p-8">
+                  <button
+                    type="button"
+                    className="absolute right-4 top-4 text-gray-400 hover:text-gray-500 sm:right-6 sm:top-8 md:right-6 md:top-6 lg:right-8 lg:top-8"
+                    onClick={close}
+                  >
+                    <span className="sr-only">Close</span>
+                    <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                  </button>
+
+                  <div className='w-full' {...rest}>
+                    {children}
+                  </div>
+                </div>
+              </DialogPanel>
+            </TransitionChild>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
+  )
+}
