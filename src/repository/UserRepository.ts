47e2b7f55e77de462f3c3fea28bd5b2dce0a545b
João Pedro Dias <joaopedro453@hotmail.com>
2024-06-01 15:52:16 -0300

    Add create method on UserRepository

--- src/repository/UserRepository.ts
+++ src/repository/UserRepository.ts
@@ -1,9 +1,19 @@
 import api from "../utils/api"
 
+interface UserCreateParams {
+  name: string
+  email: string
+  password: string
+  password_confirmation: string
+}
+
 const UserRepository = {
   index: async (): Promise<Pagination<User>> => {
     return (await api.get('/users')).data
   },
+  create: async(payload: UserCreateParams): Promise<User> => {
+    return (await api.post('/users', payload)).data
+  }
 }
 
 export default UserRepository
