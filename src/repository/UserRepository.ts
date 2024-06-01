8de068bd3f8fb0c51c8444fa1c492ee656440006
João Pedro Dias <joaopedro453@hotmail.com>
2024-06-01 11:27:19 -0300

    Add userRepository

---
+++ src/repository/UserRepository.ts
@@ -0,0 +1,9 @@
+import api from "../utils/api"
+
+const UserRepository = {
+    index: async (): Promise<Pagination<User>> => {
+        return (await api.get('/users')).data
+    },
+}
+
+export default UserRepository
